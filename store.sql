248a33581cd50c3928790b849f5290faa9aec934
kingoriwangechi <florencekingori00@gmail.com>
2024-03-02 00:34:57 +0300

    Add distributor and store tables

--- store.sql
+++ store.sql
@@ -1,13 +1,5 @@
 CREATE DATABASE movie_shop;
-
-CREATE TABLE distributor(
-distributor_id INT AUTO_INCREMENT PRIMARY KEY,
-distributor_name VARCHAR(255),
-distributor_phone_number INT,
-distributor_address INT
-);
 USE movie_shop;
-
 CREATE TABLE store(
 store_id INT AUTO_INCREMENT PRIMARY KEY,
 store_name VARCHAR(255),
